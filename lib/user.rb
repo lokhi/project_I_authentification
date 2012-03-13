13369cef90c047e198c49621cc2f0535b958b410
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-03-13 13:15:59 +0100

    Probleme de sauvegarde resolut mais bizarre

--- lib/user.rb
+++ lib/user.rb
@@ -2,11 +2,25 @@ require 'digest/sha1'
 require 'active_record'
 
 class User < ActiveRecord::Base
+
+  has_many :applications
+  has_many :uses
   validates :login, :presence => true
   validates :password, :presence => true
   validates :login, :uniqueness => true
   
+  
   def password=(clear_pass)
-    write_attribute(:password, Digest::SHA1.hexdigest(clear_pass).inspect)
+    write_attribute(:password, Digest::SHA1.hexdigest(clear_pass).inspect[1,40])
+  end
+  
+  def self.authenticate(hash)
+    user=find_by_login(hash["login"])
+    
+    if user && user.password == Digest::SHA1.hexdigest(hash["password"])
+      user
+    else
+      nil
+    end
   end
 end
