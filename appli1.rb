3856a7af3c619bd250d46805ee50f359cdbdb143
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-03-13 23:04:01 +0100

    test du decryptage du login reçu par le sauth

--- appli1.rb
+++ appli1.rb
@@ -1,15 +1,6 @@
 require 'sinatra'
 require 'securerandom'
 
-
-enable :sessions
-
-
-
-def generate_secret 
-  SecureRandom.urlsafe_base64
-end
-
 helpers do 
   def current_user
     session["current_user"]
@@ -20,6 +11,9 @@ helpers do
   end
 end
 
+def generate_secret
+  SecureRandom.urlsafe_base64
+end
 
 get '/' do
  "Application"
@@ -28,14 +22,15 @@ end
 
 
 get '/protected' do
-  if params["login"].nil? and params["secret"].nil?
-    secret=generate_secret
-    session["secret"]={secret=> Time.now.to_i}
+  if params["login"].nil? #&& params["secret"].nil?
+    secret =generate_secret
+    session[secret]=Time.now.to_i
     redirect "http://sauth/appli1/session/new?origin=/protected&secret=#{secret}"
   else
-    
-  #  if Time.now.to_i - session["secret"][params["secret"]].to_i < 5
-     session["current_user"]=params["login"]
-    #end
+    #if session[params["secret"]] && (Time.now.to_i - session[params["secret"]] < 60)
+     clogin=Base64.urlsafe_decode64(params["login"])
+     privkey=File.read('private.pem')
+     login=privkey.private_decrypt(privkey)
+
   end
 end
--- spec/appli1_spec.rb
+++ spec/appli1_spec.rb
@@ -14,37 +14,26 @@ end
 
 describe "protected area" do
   context "user not connected" do
-    it "should generate a secret and stock it into a sessions" do
-      get '/protected'
-      last_request.env["rack.session"]["secret"].nil?.should == false
-    end
-    
-    it "should redirect to sauth/appli1/session/new?origin=/protected&secret=???" do
+
+    it "should redirect to sauth/appli1/session/new?origin=/protected" do
       get '/protected'
       follow_redirect!
       last_request.params['origin'].should == "/protected"
-      last_request.params['secret'].nil?.should be_false
     end
     
     context "receiving the sauth response" do
-    
-      context "secret is not expired" do
-         it "should receive the login of the user from sauth and create his session" do
-           get '/protected', {"login"=>"toto","secret"=>"foo"},"rack.session" =>{:secret => {"foo" => "123441"}}
-           last_request.env["rack.session"]["current_user"].should == "toto"
-         end 
+      it "should decode the login in base64" do
+         Base64.should_receive(:urlsafe_decode64).with("dG90bw==")
+         get '/protected', {"login"=>"dG90bw==", "secret"=>"123"}
       end
       
-      context "secret is expired" do
-       # it "should redirect the user to the home" do
-       #    get '/protected', {"login"=>"toto","secret"=>"foo"},"rack.session" =>{:secret => {"foo" => "0"}}
-       #    follow_redirect!
-       #    last_request.path.should "/"
-       # end
+      it "should read the private key" do
+        File.should_receive(:read).with("private.pem")
+        get '/protected', {"login"=>"dG90bw==", "secret" => "123"}
       end
-    
-   
-    
+      
+      
+   	
     end
     
   end
