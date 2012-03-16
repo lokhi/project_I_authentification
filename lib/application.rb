87094609c5315c8931c78a489f41ce4b2779d6b4
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-03-16 18:50:17 +0100

    ajout de la methode de cryptage de lapplication

--- lib/application.rb
+++ lib/application.rb
@@ -1,23 +1,24 @@
 require 'active_record'
-require 'securerandom'
+require 'openssl'
 
 class Application < ActiveRecord::Base
 
   belongs_to :user
+  has_many :uses
 
   validates :name, :presence => true
   validates :adresse, :presence => true
   validates :name, :uniqueness => true
-  validates :user_id, :presence => true
+  validates :key, :presence => true
+  validates :user_id, :presence => true  
   
   
-  
-  before_save :generate_secret
-
-  def generate_secret
-     self.secret=SecureRandom.uuid
+  def self.appli_crypte_encode(appli,login)
+    key=Application.find_by_name(appli).key
+    pubkey=OpenSSL::PKey::RSA.new(key)
+    clogin=pubkey.public_encrypt(login)
+    blogin=Base64.urlsafe_encode64(clogin)
   end
-  
-  
+
   
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -13,14 +13,21 @@ describe "information missing" do
  
  it "should not be valid without a name" do
    subject.adresse="adr"
-   subject.secret="secret"
+   subject.key="123"
    subject.user_id=User.new
    subject.valid?.should == false
  end
  
  it "should not be valid without an adresse" do
    subject.name="appli1"
-   subject.secret="secret"
+   subject.key="123"
+   subject.user_id=User.new
+   subject.valid?.should == false
+ end
+ 
+  it "should not be valid without a key" do
+   subject.name="appli1"
+   subject.adresse="adr"
    subject.user_id=User.new
    subject.valid?.should == false
  end
@@ -28,7 +35,7 @@ describe "information missing" do
  it "should not be valid without an user_id" do
    subject.name="appli1"
    subject.adresse="adr"
-   subject.secret="secret"
+   subject.key="123"
    subject.valid?.should == false
  end
  
@@ -36,19 +43,53 @@ end
 
 describe "duplicate name" do
   it "should not be valid with a name who already exist" do
-    a = Application.new({"name"=>"appli1","adresse"=>"adrs","secret"=>"secret","user_id"=>User.new})
+    a = Application.new({"name"=>"appli1","adresse"=>"adrs","key"=>"1234","user_id"=>User.new})
     a.save
-    b = Application.new({"name"=>"appli1","adresse"=>"adrs2","secret"=>"secret2","user_id"=>User.new})
+    b = Application.new({"name"=>"appli1","adresse"=>"adrs2","key"=>"1234","user_id"=>User.new})
     b.valid?.should == false
   end
 end
+end
 
-describe "secret" do 
- it "should be generate" do
-   a = Application.new({"name"=>"appli1","adresse"=>"adrs","user_id"=>User.new})
-   a.save
-   a.secret.nil?.should == false
- end
+describe "encryption of a login with the application key" do
+ before(:each) do
+      @app = double(Application)
+      Application.stub(:find_by_name){@app}
+      @app.stub(:key){"-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDvqAuECzBu8JYrYleC245UZVyB
+3N11guYosh0fHaAstywPWblHwq0es92nxEtp/qin051s+48/0lu1eKQnpfhRqOIm
+xRhnyn9Vl7eR2Ssjg/yIhu+Q1nQgAnviWa6ktFbKgnxayy4Jd2a0XDsnxDWr21bQ
+mEbgekzzcZIEijHeLQIDAQAB
+-----END PUBLIC KEY-----"}
+       @pubkey = double(OpenSSL::PKey::RSA)
+       OpenSSL::PKey::RSA.stub(:new){@pubkey}
+       @pubkey.stub(:public_encrypt){"totoc"}
+       
+    end
+     
+     it "should read the private key of the application" do
+        Application.should_receive(:find_by_name).and_return(@app)
+        @app.should_receive(:key)
+        Application.appli_crypte_encode("appli","toto")
+      end
+      
+    it "should create the key with the file" do
+      OpenSSL::PKey::RSA.should_receive(:new)
+      Application.appli_crypte_encode("appli","toto")
+    end
+    
+    it "should encrypt the login" do
+      @pubkey.should_receive(:public_encrypt).with("toto")
+      Application.appli_crypte_encode("appli","toto")
+    end
+    
+    it "should encode the encrypted login" do
+      Base64.should_receive(:urlsafe_encode64)
+      Application.appli_crypte_encode("appli","toto")
+    end
 end
+
+
+
   
-end
+
