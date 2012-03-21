af97b96bafd0b390a69934a1a57eeb488731202b
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-03-21 18:45:18 +0100

    dossier tj et rm des anciens

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source 'http://rubygems.org'
-
-gem 'rspec'
-gem 'rack'
-gem 'rack-test'
-gem 'sinatra'
-gem "activerecord"
-gem "sqlite3"
--- migration.rb
+++
@@ -1,7 +0,0 @@
-$: << File.dirname(__FILE__)
-require 'database'
-
-ActiveRecord::Migration.verbose = true
-ActiveRecord::Migrator.migrate "db/migrate"
-
-
